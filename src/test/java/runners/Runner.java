e7f888b9c5819deecc26b72e30f0cdc9f10a5bf5
mert dogan <trsverige466@gmail.com>
2022-02-14 07:47:43 +0300

    son

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -6,10 +6,11 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
+        plugin={"html:target\\cucumber-reports.html"},
         features="src/test/resources/features",
         glue = "stepdefinitions",
-        tags="@Editor",
-        dryRun = true
+        tags="@parametreliTest",
+        dryRun = false
 )
 
 public class Runner {
